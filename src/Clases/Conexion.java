29c014182ec5d0f7b2f7916563e11f1e777446fd
ScarletKing101 <dariel_el_alto@yahoo.com>
2022-05-05 13:25:39 -0400

    modificando clase Conexion

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -5,26 +5,20 @@ import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
 public class Conexion {
-        Connection conectar = null;
-    
-    
-    private static final String driver ="com.mysql.cj.jdbc.Driver";
+    Connection conectar = null;
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
     private static final String user = "root";
     private static final String password = "";
     private static final String url = "jdbc:mysql://localhost:3306/prestamo_bancario";   
 
-
-    public Connection conexion()
-            
-    {
-        try{
+    public Connection conexion() {
+        try {
             Class.forName(driver);
             conectar = DriverManager.getConnection(url, user, password);
             System.out.println("Conexion Establecida");
             //JOptionPane.showMessageDialog(null,"Conexion Establecida");
             
-        }catch (Exception e){
-        
+        } catch (Exception e){
             JOptionPane.showMessageDialog(null,"ERROR: "+e);
         }
         return conectar;
